1f31d4db78022b4ad83a7e07219e8f248fce8a7e
Jon Wood <jwood803@users.noreply.github.com>
2018-12-30 16:50:08 -0500

    Add recommender sample (#2)

---
+++ MLNetExamples/BookRecommendations/BookRatingPrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRecommendations
+{
+    public class BookRatingPrediction
+    {
+        public float Label;
+
+        public float Score;
+    }
+}
