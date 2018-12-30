ad0d4ef7216538ccda3011923f8908cd5c5d4c74
J W <woodjon@gmail.com>
2018-12-30 13:54:36 -0500

    Add recommender sample

---
+++ MLNetExamples/BookRecommendations/BookRating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRecommendations
+{
+    public class BookRating
+    {
+        public float Label;
+
+        public float user;
+
+        public float bookid;
+    }
+}
