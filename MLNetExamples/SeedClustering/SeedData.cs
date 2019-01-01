740cde21524a2d19e1282c88cdb814d0a3812e2a
J W <woodjon@gmail.com>
2018-12-31 23:50:10 -0500

    Add clustering example

---
+++ MLNetExamples/SeedClustering/SeedData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeedClustering
+{
+    public class SeedData
+    {
+        public float A;
+        public float P;
+        public float C;
+        public float LK;
+        public float WK;
+        public float A_Coef;
+        public float LKG;
+        public float Label;
+    }
+}
