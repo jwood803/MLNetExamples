ed699ba19755254fa0f95a94670db5b8ab7f9490
J W <woodjon@gmail.com>
2019-08-08 18:43:39 -0400

    Update F# sample

--- MLNetExamples/FSharpRegression/Program.fs
+++ MLNetExamples/FSharpRegression/Program.fs
@@ -7,27 +7,36 @@ let testFile = "./SalaryTest.csv"
 
 type SalaryData = {
   [<LoadColumn(0)>]
-  YearsExperience: float32
+  YearsExperience: float32;
 
   [<LoadColumn(1)>]
-  Salary: float32
+  Salary: float32;
+}
+
+[<CLIMutable>]
+type SalaryPrediction = {
+  [<ColumnName("Score")>]
+  PredictedSalary: float32;
 }
 
 [<EntryPoint>]
 let main argv =
     let context = MLContext()
 
-    let trainData = context.Data.LoadFromTextFile<SalaryData>(trainFile, hasHeader=true, separatorChar=',')
-    let testData = context.Data.LoadFromTextFile<SalaryData>(testFile, hasHeader=true, separatorChar=',')
+    let loadDataByPath path =
+      context.Data.LoadFromTextFile<SalaryData>(path, hasHeader=true, separatorChar=',')
 
-    // let trainData = loadDataByPath trainFile
-    //let testData = loadDataByPath testFile
+    //let trainData = context.Data.LoadFromTextFile<SalaryData>(trainFile, hasHeader=true, separatorChar=',')
+    //let testData = context.Data.LoadFromTextFile<SalaryData>(testFile, hasHeader=true, separatorChar=',')
+
+    let trainData = loadDataByPath trainFile
+    let testData = loadDataByPath testFile
 
     let pipeline = 
       EstimatorChain()
         .Append(context.Transforms.Concatenate("Features", "YearsExperience"))
         .Append(context.Transforms.CopyColumns(("Label", "Salary")))
-        .Append(context.Regression.Trainers.Sdca("Label", "Features"))
+        .Append(context.Regression.Trainers.LbfgsPoissonRegression())
 
     printfn "Training model..."
     let model = pipeline.Fit trainData
@@ -38,6 +47,18 @@ let main argv =
     let metrics = context.Regression.Evaluate(predictions, "Label", "Score")
 
     printfn "RMS - %.2f" metrics.RootMeanSquaredError
+    printfn "R^2 - %.2f" metrics.RSquared
+
+    let predictionFunc = context.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(model)
+
+    let salaryPrediction = { 
+      YearsExperience = 8.0f 
+      Salary = 0.0f
+    }
+
+    let prediction = predictionFunc.Predict(salaryPrediction)
+
+    printfn "Prediction - %.2f" prediction.PredictedSalary
 
     Console.ReadLine() |> ignore
 
