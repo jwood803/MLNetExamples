3408172e189c2580f8bbe866383192139291b641
J W <woodjon@gmail.com>
2019-08-20 05:55:30 -0400

    Add feature importance project

---
+++ MLNetExamples/FeatureImportance/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML;
+using System;
+using System.Linq;
+
+namespace FeatureImportance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true, separatorChar: ',');
+
+            var preview = data.Preview();
+
+            var features = data.Schema
+                .Select(col => col.Name)
+                .Where(colName => colName != "Label" && colName != "OceanProximity")
+                .ToArray();
+
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(data);
+
+            //context.
+        }
+    }
+}
