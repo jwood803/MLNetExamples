87dc9d7571708b2dc64abda5f3a519332ccfac51
J W <woodjon@gmail.com>
2019-09-10 21:38:27 -0400

    Update database loader sample

--- MLNetExamples/DatabaseLoader/Program.cs
+++ MLNetExamples/DatabaseLoader/Program.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 
 namespace DbLoader
 {
@@ -22,8 +23,16 @@ namespace DbLoader
 
             var loaderColumns = new DatabaseLoader.Column[]
             {
-                new DatabaseLoader.Column() { Name = "YearsOfExperience", Type = DbType.Single },
-                new DatabaseLoader.Column() { Name = "Salary", Type = DbType.Single }
+                new DatabaseLoader.Column() { Name = "Longitude", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "Latitude", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "HousingMedianAge", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "TotalRooms", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "TotalBedrooms", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "Population", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "Households", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "MedianIncome", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "MedianHouseValue", Type = DbType.Single },
+                new DatabaseLoader.Column() { Name = "OceanProximity", Type = DbType.String }
             };
 
             var connection = new SqlConnection(connectionString);
@@ -34,23 +43,44 @@ namespace DbLoader
             var loader = context.Data.CreateDatabaseLoader(loaderColumns);
 
             var dbSource = new DatabaseSource(factory, connectionString,
-                "SELECT YearsOfExperience, Salary FROM salarydb.dbo.SalaryData");
+                "SELECT * FROM salarydb.dbo.Housing");
 
             var data = loader.Load(dbSource);
 
             var preview = data.Preview();
 
-            var testTrainData = context.Data.TrainTestSplit(data, testFraction: 0.2);
+            var testTrainSplit = context.Data.TrainTestSplit(data, testFraction: 0.2);
 
-            var pipeline = context.Transforms.Concatenate("Features", "YearsOfExperience")
-                .Append(context.Transforms.CopyColumns("Label", "Salary"))
-                .Append(context.Regression.Trainers.Sdca());
+            var features = data.Schema
+                    .Select(col => col.Name)
+                    .Where(colName => colName != "MedianHouseValue" && colName != "OceanProximity")
+                    .ToArray();
 
-            var model = pipeline.Fit(testTrainData.TrainSet);
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression(featureColumnName: "Features", labelColumnName: "MedianHouseValue"));
 
-            var prediction = model.Transform(testTrainData.TestSet);
+            var model = pipeline.Fit(testTrainSplit.TestSet);
 
-            var metrics = context.Regression.Evaluate(prediction);
+            var predictionFunc = context.Model.CreatePredictionEngine<HousingData, HousingPrediction>(model);
+
+            var prediction = predictionFunc.Predict(new HousingData
+            {
+                Longitude = -122.25f,
+                Latitude = 37.85f,
+                HousingMedianAge = 55.0f,
+                TotalRooms = 1627.0f,
+                TotalBedrooms = 235.0f,
+                Population = 322.0f,
+                Households = 120.0f,
+                MedianIncome = 8.3014f,
+                OceanProximity = "NEAR BAY"
+            });
+
+            Console.WriteLine($"Prediction - {prediction.PredictedHouseValue}");
+
+            Console.ReadLine();
         }
     }
 }
