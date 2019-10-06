785f3b26291366ec3dc36a18223331c4905de161
J W <woodjon@gmail.com>
2019-10-06 18:38:37 -0400

    Build model for scaled data sample

--- MLNetExamples/ScaleData/Program.cs
+++ MLNetExamples/ScaleData/Program.cs
@@ -15,14 +15,42 @@ namespace ScaleData
             var columns = data.Schema
                 .Select(col => col.Name)
                 .Where(col => col != "Label" && col != "OceanProximity")
-                .Select(col => new InputOutputColumnPair($"{col}_scaled", col))
+                .Select(col => new InputOutputColumnPair(col, col))
                 .ToArray();
 
             var scaling = context.Transforms.NormalizeMinMax(columns);
 
             var dataScaled = scaling.Fit(data).Transform(data);
 
+            var features = columns.Select(col => col.OutputColumnName).ToArray();
+
             var preview = dataScaled.Preview();
+
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(dataScaled);
+
+            var predictionFunc = context.Model.CreatePredictionEngine<HousingData, HousingPrediction>(model);
+
+            var prediction = predictionFunc.Predict(new HousingData
+            {
+                Longitude = -122.25f,
+                Latitude = 37.85f,
+                HousingMedianAge = 55.0f,
+                TotalRooms = 1627.0f,
+                TotalBedrooms = 235.0f,
+                Population = 322.0f,
+                Households = 120.0f,
+                MedianIncome = 8.3014f,
+                OceanProximity = "NEAR BAY"
+            });
+
+            Console.WriteLine($"Prediction - {prediction.PredictedHouseValue}");
+
+            Console.ReadLine();
         }
     }
 }
