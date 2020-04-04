e78c458867cd2bbbda97069dd263e6c04320e40c
J W <woodjon@gmail.com>
2020-04-04 00:47:10 -0400

    Add event hub consumer project

---
+++ MLNetExamples/EventHubPredict/HousingData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.ML.Data;
+using System.Text.Json.Serialization;
+
+namespace EventHubPredict
+{
+    public class HousingData
+    {
+        [LoadColumn(0)]
+        [JsonPropertyName("longitude")]
+        public float Longitude { get; set; }
+
+        [LoadColumn(1)]
+        [JsonPropertyName("latitude")]
+        public float Latitude { get; set; }
+
+        [LoadColumn(2)]
+        [JsonPropertyName("housing_median_age")]
+        public float HousingMedianAge { get; set; }
+
+        [LoadColumn(3)]
+        [JsonPropertyName("total_rooms")]
+        public float TotalRooms { get; set; }
+
+        [LoadColumn(4)]
+        [JsonPropertyName("total_bedrooms")]
+        public float TotalBedrooms { get; set; }
+
+        [LoadColumn(5)]
+        [JsonPropertyName("population")]
+        public float Population { get; set; }
+
+        [LoadColumn(6)]
+        [JsonPropertyName("households")]
+        public float Households { get; set; }
+
+        [LoadColumn(7)]
+        [JsonPropertyName("median_income")]
+        public float MedianIncome { get; set; }
+
+        [LoadColumn(8), ColumnName("Label")]
+        public float MedianHouseValue { get; set; }
+
+        [LoadColumn(9)]
+        [JsonPropertyName("ocean_proximity")]
+        public string OceanProximity { get; set; }
+    }
+}
--- MLNetExamples/EventHubPredict/Program.cs
+++ MLNetExamples/EventHubPredict/Program.cs
@@ -1,18 +1,65 @@
-﻿using Microsoft.Azure.EventHubs;
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using Microsoft.ML;
 using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace EventHubPredict
 {
     class Program
     {
-        private static EventHubClient client;
-        private const string EventHubConnectionString = "{Event Hubs connection string}";
-        private const string EventHubName = "{Event Hub path/name}";
+        private const string EventHubConnectionString = "Endpoint=sb://mlnetehn.servicebus.windows.net/;SharedAccessKeyName=Manage;SharedAccessKey=6OZXDj5GE1Z+yTc1fVlzogRQL/6YlmIKnoZwx5wZJOk=;EntityPath=mlneteh";
+        private const string EventHubName = "mlneteh ";
+        private const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=databricksdemostorage;AccountKey=+z/ldXQTw4r4pWaEHFg8W+6Sl8AraGQzrN9w3XgRrWiM11PTOcuYjtLrrKBULVf4tTILfyklWxL8DoKilBBEYg==;BlobEndpoint=https://databricksdemostorage.blob.core.windows.net/;QueueEndpoint=https://databricksdemostorage.queue.core.windows.net/;TableEndpoint=https://databricksdemostorage.table.core.windows.net/;FileEndpoint=https://databricksdemostorage.file.core.windows.net/;";
+        private const string blobContainerName = "events";
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
 
-            Console.WriteLine("Hello World!");
+            var storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+
+            var processor = new EventProcessorClient(storageClient, consumerGroup, EventHubConnectionString, EventHubName);
+
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            await processor.StartProcessingAsync();
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            await processor.StopProcessingAsync();
+        }
+
+        private static Task ProcessErrorHandler(ProcessErrorEventArgs arg)
+        {
+            Console.WriteLine($"\tPartition '{ arg.PartitionId}': an unhandled exception was encountered.");
+            Console.WriteLine(arg.Exception.Message);
+
+            return Task.CompletedTask;
+        }
+
+        private static Task ProcessEventHandler(ProcessEventArgs arg)
+        {
+            var payload = Encoding.UTF8.GetString(arg.Data.Body.ToArray());
+
+            var data = JsonSerializer.Deserialize<HousingData>(payload);
+
+            var context = new MLContext();
+
+            var model = context.Model.Load("./Model/housing-model.zip", out DataViewSchema inputSchema);
+
+            var predictionEngine = context.Model.CreatePredictionEngine<HousingData, HousingPrediction>(model, inputSchema: inputSchema);
+
+            var prediction = predictionEngine.Predict(data);
+
+            Console.WriteLine($"Prediction is {prediction.PredictedHouseValue}");
+
+            return Task.CompletedTask;
         }
     }
 }
