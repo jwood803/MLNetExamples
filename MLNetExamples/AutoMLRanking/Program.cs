4a68602dce89461fe270699a3fee02263cfc69b4
J W <woodjon@gmail.com>
2020-08-26 05:27:24 -0400

    Add additional projects

---
+++ MLNetExamples/AutoMLRanking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+
+namespace AutoMLRanking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<RankingData>("./ranking.tsv", separatorChar: '\t');
+
+            var settings = new RankingExperimentSettings
+            {
+                MaxExperimentTimeInSeconds = 300,
+                OptimizingMetric = RankingMetric.Ndcg
+            };
+
+            var experiment = context.Auto().CreateRankingExperiment(settings);
+
+            var results = experiment.Execute(data);
+
+            var bestModel = results.BestRun.Model;
+        }
+    }
+}
