20f8d6826adf5b1051aeb3bee2fcc5cf83daa4f3
J W <woodjon@gmail.com>
2020-09-23 06:55:51 -0400

    Update sample

--- MLNetExamples/AutoMLRanking/Program.cs
+++ MLNetExamples/AutoMLRanking/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.ML;
 using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
 
 namespace AutoMLRanking
 {
@@ -12,17 +14,33 @@ namespace AutoMLRanking
 
             var data = context.Data.LoadFromTextFile<RankingData>("./ranking.tsv", separatorChar: '\t');
 
+            var trainTestSplit = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
             var settings = new RankingExperimentSettings
             {
                 MaxExperimentTimeInSeconds = 300,
-                OptimizingMetric = RankingMetric.Ndcg
+                OptimizingMetric = RankingMetric.Ndcg,
             };
 
             var experiment = context.Auto().CreateRankingExperiment(settings);
 
-            var results = experiment.Execute(data);
+            var progressHandler = new Progress<RunDetail<RankingMetrics>>(ph =>
+            {
+                if (ph.ValidationMetrics != null)
+                {
+                    Console.WriteLine($"Current trainer - {ph.TrainerName} with nDCG {ph.ValidationMetrics.NormalizedDiscountedCumulativeGains.Average()}");
+                }
+            });
 
-            var bestModel = results.BestRun.Model;
+            var results = experiment.Execute(trainTestSplit.TrainSet, validationData: trainTestSplit.TestSet, 
+                progressHandler: progressHandler);
+
+            var bestRun = results.BestRun;
+
+            var metrics = bestRun.ValidationMetrics.NormalizedDiscountedCumulativeGains;
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"Best model {bestRun.TrainerName} - with nDCG {metrics.Average()}");
         }
     }
 }
